5273c6829a63e262d7285e9d1f8841d63a0512e1
Nathalie Flores <21250@student.dorset-college.ie>
2021-01-26 09:33:27 -0600

    second commit -

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,36 +1,52 @@
 package com.jetbrains;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        try {
+        final String DB_URL = "jdbc:mysql://localhost:3306/titanicmanifest";
+        final String DB_USER = "Nat";
+        final String DB_PASSWORD = "m+S0sh1@tp/6";
+
+        try  {
             Class.forName("com.mysql.jdbc.Driver");
 
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/titanicmanifest", "Nat", "m+S0sh1@tp/6");
+                    DB_URL, DB_USER, DB_PASSWORD);
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(" SELECT * FROM passengers");
+            String sqlQuery = "select * from titanic";
 
-            System.out.println("before while");
+            ResultSet rs = stmt.executeQuery(sqlQuery);
+
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            for(int i = 1; i<= numberOfColumns; i++) {
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            }
+            System.out.println();
+
 
             while (rs.next()) {
-                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(4));
+                // System.out.println(rs.getInt(1) + "\t  " + rs.getString(2) + " \t " + rs.getString(3));
+                System.out.println(rs.getInt(1) + "\t  " + rs.getInt(2) + " \t " + rs.getInt(3) + " \t " + rs.getString(4));
             }
-            System.out.println("after while");
 
-            con.close();
 
-        } catch (Exception e) {
+
+
+        } catch (SQLException  e) {
             System.out.println(e.toString());
 
-        } finally {
+        } catch(Exception e) {
+
+        } finally{
+            // System.out.println("finally");
         }
+
+
     }
 }
